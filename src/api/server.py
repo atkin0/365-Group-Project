e9f5f373f2a3b1a4e615cacecbb936d4d65f8072
Tyler-Dang <waztec1128@gmail.com>
2025-05-05 10:37:14 -0700

    Add Src

---
+++ src/api/server.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+from src.api import admin
+from starlette.middleware.cors import CORSMiddleware
+
+description = """
+Central Coast Cauldrons is the premier ecommerce site for all your alchemical desires.
+"""
+tags_metadata = [
+]
+
+app = FastAPI(
+    title="Gamer Society",
+    description=description,
+    version="0.0.1",
+    terms_of_service="http://example.com/terms/",
+    contact={
+        "name": "Tyler Dang",
+        "email": "tbdang@calpoly.edu",
+    },
+    openapi_tags=tags_metadata,
+)
+
+origins = ["https://potion-exchange.vercel.app"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "OPTIONS"],
+    allow_headers=["*"],
+)
+
+
+
+
+@app.get("/")
+async def root():
+    return {"message": "Shop is open for business!"}
