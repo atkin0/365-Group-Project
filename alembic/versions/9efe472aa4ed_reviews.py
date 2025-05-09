0f9377ff9a7a7370bde707eed2bcb10e27bc35ca
Atkin Rong <tyratus1318@gmail.com>
2025-05-09 10:33:16 -0700

    added datetime to some tables

--- alembic/versions/9efe472aa4ed_reviews.py
+++ alembic/versions/9efe472aa4ed_reviews.py
@@ -9,7 +9,7 @@ from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
-
+from sqlalchemy.dialects.mysql import DATETIME
 
 # revision identifiers, used by Alembic.
 revision: str = '9efe472aa4ed'
@@ -28,7 +28,7 @@ def upgrade() -> None:
         sa.Column("published", sa.Boolean, server_default=sa.sql.expression.literal(False), nullable=False),
         sa.Column("user_id", sa.Integer, nullable=False),
         sa.Column("game_id", sa.Integer, nullable=False),
-
+        sa.Column("updated_at", sa.DateTime(timezone=True), default=sa.func.now, onupdate=sa.func.now, nullable=False),
     )
 
     op.create_table(
@@ -48,14 +48,14 @@ def upgrade() -> None:
     op.create_table(
         "games",
         sa.Column("id", sa.Integer, primary_key=True),
-        sa.Column("name", sa.String, nullable=False),
+        sa.Column("game", sa.String, nullable=False),
         sa.Column("genre_id", sa.Integer, nullable=False),
     )
 
     op.create_table(
         "genres",
         sa.Column("id", sa.Integer, primary_key=True),
-        sa.Column("name", sa.String, nullable=False),
+        sa.Column("genre", sa.String, nullable=False),
     )
 
     op.create_table(
@@ -63,14 +63,17 @@ def upgrade() -> None:
         sa.Column("id", sa.Integer, primary_key=True),
         sa.Column("review_name", sa.String, nullable=False),
         sa.Column("optional_rating", sa.Integer, nullable=False),
-        sa.Column("review_id", sa.Integer, nullable=False)
+        sa.Column("review_id", sa.Integer, nullable=False),
+        sa.Column("updated_at", sa.DateTime(timezone=True), default=sa.func.now, onupdate=sa.func.now, nullable=False),
+
     )
 
     op.create_table(
-        "History",
+        "history",
         sa.Column("user_id", sa.Integer, primary_key=True),
         sa.Column("game_id", sa.Integer, primary_key=True),
-        sa.Column("time_played", sa.Float, primary_key=True),
+        sa.Column("time_played", sa.Float, nullable=False),
+        sa.Column("last_played", sa.DateTime(timezone=True), default=sa.func.now, onupdate=sa.func.now, nullable=False),
     )
 
     pass
