bd3383b184b4b5ba0a5be82cd669369123440fcf
RishanBaweja <rbaweja@calpoly.edu>
2025-06-01 22:19:38 -0700

    fixes to settings

---
+++ alembic/versions/bfff6148d0b3_fix_user_privacy.py
@@ -0,0 +1,31 @@
+"""Fix user privacy
+
+Revision ID: bfff6148d0b3
+Revises: 91ed5646b8ad
+Create Date: 2025-06-01 01:41:33.871472
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'bfff6148d0b3'
+down_revision: Union[str, None] = '91ed5646b8ad'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.drop_column("users","private_account")
+
+    #default to True to protect user settings
+    op.add_column("users",sa.Column("account_is_private", sa.Boolean(),nullable=False,server_default=sa.text("true")))
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_column("users","account_is_private")
